8f342ed7118c9bc6df21c2fae15c33730b6f9815
m1ch14l <hellinxmichiel@hotmail.com>
2015-12-14 12:06:45 +0100

    v1
    Added v1 files

---
+++ pgReminder/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace pgReminder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_input = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.c_enabled = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.c_time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.maskedTextBox_time = new System.Windows.Forms.MaskedTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Inhoud pop-up";
+            // 
+            // textBox_input
+            // 
+            this.textBox_input.Location = new System.Drawing.Point(12, 26);
+            this.textBox_input.Multiline = true;
+            this.textBox_input.Name = "textBox_input";
+            this.textBox_input.Size = new System.Drawing.Size(275, 66);
+            this.textBox_input.TabIndex = 1;
+            this.textBox_input.Text = "scarface0359 is awesome!";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.c_enabled,
+            this.c_time});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 150);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(275, 153);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // c_enabled
+            // 
+            this.c_enabled.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.c_enabled.FillWeight = 35F;
+            this.c_enabled.HeaderText = "Ingeschakeld";
+            this.c_enabled.Name = "c_enabled";
+            this.c_enabled.TrueValue = "1";
+            // 
+            // c_time
+            // 
+            this.c_time.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.c_time.FillWeight = 65F;
+            this.c_time.HeaderText = "Tijdstip";
+            this.c_time.Name = "c_time";
+            // 
+            // maskedTextBox_time
+            // 
+            this.maskedTextBox_time.Location = new System.Drawing.Point(168, 124);
+            this.maskedTextBox_time.Mask = "00:00";
+            this.maskedTextBox_time.Name = "maskedTextBox_time";
+            this.maskedTextBox_time.PromptChar = '0';
+            this.maskedTextBox_time.Size = new System.Drawing.Size(38, 20);
+            this.maskedTextBox_time.TabIndex = 3;
+            this.maskedTextBox_time.ValidatingType = typeof(System.DateTime);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nieuw tijdstip";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(212, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 315);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.maskedTextBox_time);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox_input);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(315, 354);
+            this.MinimumSize = new System.Drawing.Size(315, 354);
+            this.Name = "Form1";
+            this.Text = "PG\'s awesome reminder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_input;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox_time;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn c_enabled;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c_time;
+    }
+}
+
---
+++ pgReminder/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pgReminder
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer1;
+        private Timer timer2;
+        private Boolean alreadySeen = false;
+
+        [DllImport("User32.dll")]
+        public static extern Int32 SetForegroundWindow(int hWnd);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String newTime = maskedTextBox_time.Text;
+            newTime = newTime.Replace(' ', '0');
+            while(newTime.Length < 5)
+            {
+                newTime = newTime + "0";
+            }
+            this.dataGridView1.Rows.Add(1, newTime);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitTimer();
+
+            
+        }
+
+        public void InitTimer()
+        {
+            timer1 = new Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 1000;
+            timer1.Start();
+
+            timer2 = new Timer();
+            timer2.Tick += new EventHandler(timer2_Tick);
+            timer2.Interval = 60100;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            alreadySeen = false;
+            timer2.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.TopMost = false;
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if(row.Cells["c_enabled"].Value != null && alreadySeen == false) { 
+                    String rowTime = row.Cells["c_time"].Value.ToString();
+                    String curTime = DateTime.Now.ToString("HH:mm");
+                    textBox_input.Text = DateTime.Now.ToString() + "> " + rowTime + " - " + curTime;
+                    if (rowTime.Equals(curTime))
+                    {
+                        showMessage();
+                    }
+                }
+            }
+        }
+
+        private void showMessage()
+        {
+            if (alreadySeen == false) {
+                alreadySeen = true;
+
+                SetForegroundWindow(Handle.ToInt32());
+                this.TopMost = true;
+                this.Focus();
+
+                System.Media.SystemSounds.Asterisk.Play();
+                MessageBox.Show(textBox_input.Text,
+                    "PG's reminder",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information,
+                    MessageBoxDefaultButton.Button1);
+
+                timer2.Start();
+            }
+        }
+    }
+}
